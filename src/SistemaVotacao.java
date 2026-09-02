33fbaf2b2aefa6b59ca7868f6ec0e2e120893b47
larissa rosario <larissa.11766579@aluno.mg.gov.br>
2026-09-02 11:10:00 -0300

    feat: listar candidatos disponíveis para votação

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -29,6 +29,16 @@ public class SistemaVotacao {
 
         return indiceEncontrado;
     }
+    static void mostrarCandidatos() {
+        System.out.println("\nCandidatos disponíveis:");
+
+        for (int i = 0; i < quantidadeCandidatos; i++) {
+            System.out.println(
+                    numerosCandidatos[i] + " - " + nomesCandidatos[i]
+            );
+        }
+    }
+
     public static void main(String[] args){
         int opcao;
 
